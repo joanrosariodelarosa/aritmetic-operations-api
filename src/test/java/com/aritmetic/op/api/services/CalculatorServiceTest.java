129f14d8078510a67d1714c7a50a5260a56032fe
Joan <joan.rosario@gobio.com>
2023-06-26 11:10:22 -0400

    aritmetic-api : enable unit test

--- src/test/java/com/aritmetic/op/api/services/CalculatorServiceTest.java
+++ src/test/java/com/aritmetic/op/api/services/CalculatorServiceTest.java
@@ -23,110 +23,110 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 class CalculatorServiceTest {
 
-//    @InjectMocks
-//    private CalculatorServiceImpl calculatorServiceImpl;
-//
-//    @InjectMocks
-//    private RandomStringServiceImpl randomStringService;
-//
-//    @InjectMocks
-//    private UserServiceImpl userServiceImpl;
-//
-//    @BeforeEach
-//    private void setup() {
-//        calculatorServiceImpl = new CalculatorServiceImpl(randomStringService, userServiceImpl);
-//    }
-//
-//    @Test
-//    void testSuccessfullyAdditionOperationAndStatusCode() {
-//
-//        ResponseEntity<OperationResponseDto> result = calculatorServiceImpl
-//                .performAddition(Arrays.asList(1.5, 2.5));
-//
-//        assertEquals(getFinalResponse(4.0, 0.0, null), result.getBody());
-//        assertEquals(HttpStatus.OK, result.getStatusCode());
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("genericErrorMessages")
-//    void testAdditionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
-//        try {
-//            calculatorServiceImpl.performAddition(numbers);
-//        } catch (CalculatorException e) {
-//            assertMessageByOperationType(OperationType.ADDITION, expectedErrorMessage, e);
-//        }
-//    }
-//    @ParameterizedTest
-//    @MethodSource("genericErrorMessages")
-//    void testSubtractionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
-//        try {
-//            calculatorServiceImpl.performSubtraction(numbers);
-//        } catch (CalculatorException e) {
-//            assertMessageByOperationType(OperationType.SUBTRACTION, expectedErrorMessage, e);
-//        }
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource({"genericErrorMessages"})
-//    void testMultiplicationOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
-//        try {
-//            calculatorServiceImpl.performMultiplication(numbers);
-//        } catch (CalculatorException e) {
-//            assertMessageByOperationType(OperationType.MULTIPLICATION, expectedErrorMessage, e);
-//        }
-//    }
-//    @ParameterizedTest
-//    @MethodSource({"divisionErrorMessages", "genericErrorMessages"})
-//    void testDivisionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
-//        try {
-//            calculatorServiceImpl.performDivision(numbers);
-//        } catch (CalculatorException e) {
-//            assertMessageByOperationType(OperationType.DIVISION, expectedErrorMessage, e);
-//        }
-//    }
-//    @ParameterizedTest
-//    @MethodSource({"squareRootErrorMessages", "genericErrorMessages"})
-//    void testSquareRootOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
-//        try {
-//            calculatorServiceImpl.performSquareRoot(numbers);
-//        } catch (CalculatorException e) {
-//            assertMessageByOperationType(OperationType.SQUARE_ROOT, expectedErrorMessage, e);
-//        }
-//    }
-//
-//    private static List<Object[]> genericErrorMessages() {
-//        return Arrays.asList(
-//                new Object[]{Arrays.asList(1.5), AT_LEAST_TWO_NUMBERS_ERROR},
-//                new Object[]{Arrays.asList(2.0, ""), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE},
-//                new Object[]{Arrays.asList(), EMPTY_NUMBERS_ERROR_MESSAGE},
-//                new Object[]{Arrays.asList(2.0, 2.0, 2.0), ONLY_TWO_NUMBERS_ALLOWED_MESSAGE},
-//                new Object[]{Arrays.asList(2.0, 2), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE},
-//                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
-//    }
-//    private static List<Object[]> divisionErrorMessages() {
-//        return Arrays.asList(
-//                new Object[]{Arrays.asList(5.0, 0.0), DIVIDE_BY_ZERO},
-//                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
-//    }
-//    private static List<Object[]> squareRootErrorMessages() {
-//        return Arrays.asList(
-//                new Object[]{Arrays.asList(5.0, 0.0), ERROR_JUST_ONE_NUMBER},
-//                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
-//    }
-//
-//    private void assertMessageByOperationType(OperationType operationType,
-//                                              String expectedErrorMessage, CalculatorException e) {
-//        Assertions.assertEquals(expectedErrorMessage + " in "
-//                + operationType.toString().toLowerCase(), e.getMessage());
-//    }
-//
-//    private OperationResponseDto getFinalResponse(double result, double currentBalance, String randomString) {
-//        return OperationResponseDto.builder()
-//                .success(true)
-//                .result(result)
-//                .currentBalance(currentBalance)
-//                .randomString(randomString)
-//                .errorMessage(SUCCESSFULLY_OPERATION)
-//                .build();
-//    }
+    @InjectMocks
+    private CalculatorServiceImpl calculatorServiceImpl;
+
+    @InjectMocks
+    private RandomStringServiceImpl randomStringService;
+
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+
+    @BeforeEach
+    private void setup() {
+        calculatorServiceImpl = new CalculatorServiceImpl(randomStringService, userServiceImpl);
+    }
+
+    @Test
+    void testSuccessfullyAdditionOperationAndStatusCode() {
+
+        ResponseEntity<OperationResponseDto> result = calculatorServiceImpl
+                .performAddition(Arrays.asList(1.5, 2.5));
+
+        assertEquals(getFinalResponse(4.0, 0.0, null), result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @ParameterizedTest
+    @MethodSource("genericErrorMessages")
+    void testAdditionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
+        try {
+            calculatorServiceImpl.performAddition(numbers);
+        } catch (CalculatorException e) {
+            assertMessageByOperationType(OperationType.ADDITION, expectedErrorMessage, e);
+        }
+    }
+    @ParameterizedTest
+    @MethodSource("genericErrorMessages")
+    void testSubtractionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
+        try {
+            calculatorServiceImpl.performSubtraction(numbers);
+        } catch (CalculatorException e) {
+            assertMessageByOperationType(OperationType.SUBTRACTION, expectedErrorMessage, e);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource({"genericErrorMessages"})
+    void testMultiplicationOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
+        try {
+            calculatorServiceImpl.performMultiplication(numbers);
+        } catch (CalculatorException e) {
+            assertMessageByOperationType(OperationType.MULTIPLICATION, expectedErrorMessage, e);
+        }
+    }
+    @ParameterizedTest
+    @MethodSource({"divisionErrorMessages", "genericErrorMessages"})
+    void testDivisionOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
+        try {
+            calculatorServiceImpl.performDivision(numbers);
+        } catch (CalculatorException e) {
+            assertMessageByOperationType(OperationType.DIVISION, expectedErrorMessage, e);
+        }
+    }
+    @ParameterizedTest
+    @MethodSource({"squareRootErrorMessages", "genericErrorMessages"})
+    void testSquareRootOperationErrorMessages(List<Double> numbers, String expectedErrorMessage) {
+        try {
+            calculatorServiceImpl.performSquareRoot(numbers);
+        } catch (CalculatorException e) {
+            assertMessageByOperationType(OperationType.SQUARE_ROOT, expectedErrorMessage, e);
+        }
+    }
+
+    private static List<Object[]> genericErrorMessages() {
+        return Arrays.asList(
+                new Object[]{Arrays.asList(1.5), AT_LEAST_TWO_NUMBERS_ERROR},
+                new Object[]{Arrays.asList(2.0, ""), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE},
+                new Object[]{Arrays.asList(), EMPTY_NUMBERS_ERROR_MESSAGE},
+                new Object[]{Arrays.asList(2.0, 2.0, 2.0), ONLY_TWO_NUMBERS_ALLOWED_MESSAGE},
+                new Object[]{Arrays.asList(2.0, 2), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE},
+                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
+    }
+    private static List<Object[]> divisionErrorMessages() {
+        return Arrays.asList(
+                new Object[]{Arrays.asList(5.0, 0.0), DIVIDE_BY_ZERO},
+                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
+    }
+    private static List<Object[]> squareRootErrorMessages() {
+        return Arrays.asList(
+                new Object[]{Arrays.asList(5.0, 0.0), ERROR_JUST_ONE_NUMBER},
+                new Object[]{Arrays.asList(5, 0.0), ONLY_DOUBLE_NUMBERS_ERROR_MESSAGE});
+    }
+
+    private void assertMessageByOperationType(OperationType operationType,
+                                              String expectedErrorMessage, CalculatorException e) {
+        Assertions.assertEquals(expectedErrorMessage + " in "
+                + operationType.toString().toLowerCase(), e.getMessage());
+    }
+
+    private OperationResponseDto getFinalResponse(double result, double currentBalance, String randomString) {
+        return OperationResponseDto.builder()
+                .success(true)
+                .result(result)
+                .currentBalance(currentBalance)
+                .randomString(randomString)
+                .errorMessage(SUCCESSFULLY_OPERATION)
+                .build();
+    }
 }
