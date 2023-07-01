d134ae5865c26b2f6e1f016330ac0301a858b339
Joan <joan.rosario@gobio.com>
2023-07-01 12:46:26 -0400

    aritmetic-api : fixing logout functionality

---
+++ src/main/java/com/aritmetic/op/api/dtos/OperationRequestDto.java
@@ -0,0 +1,19 @@
+package com.aritmetic.op.api.dtos;
+
+import com.aritmetic.op.api.types.OperationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OperationRequestDto {
+    private List<Double> numbers;
+    private OperationType operationType;
+
+}
